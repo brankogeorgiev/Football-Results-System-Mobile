54058c604f2b9804f076528b454b0f9db1a57def
Branko Georgiev <bane.g2002@gmail.com>
2026-01-27 11:47:34 +0100

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/brankogeorgiev/Platform.kt
@@ -0,0 +1,7 @@
+package com.brankogeorgiev
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
---
+++ composeApp/src/iosMain/kotlin/com/brankogeorgiev/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.brankogeorgiev
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
